58b6aba6ff9133ff2e5946d69c37b0d45d240ab3
Jason <jason@bydesignpublishing.com>
2022-07-06 09:31:55 -0700

    remove unused methods. update output tests

--- colorful/__init__.py
+++ colorful/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """Color terminal text with ANSI Styles and Colors"""
 
+__author__ = "Jason Rebuck"
+__copyright__ = "2021-2022"
+__version__ = "0.22"
+
 # NOTE:
 # reset = \33[0m
 # color = \33[ + 3 + COLOR NUMBER + m
@@ -8,236 +12,218 @@
 # style = \33[ + STYLE NUMBER + m
 # bg = \33[ + 4 + COLOR NUMBER + m
 
-__author__ = "Jason Rebuck"
-__copyright__ = "2021-2022"
-__version__ = "0.20"
+# COLORS
+# black = 0
+# red = 1
+# green = 2
+# yellow = 3
+# blue = 4
+# magenta = 5
+# cyan = 6
+# white = 7
+
+# STYLES
+# reset = 0
+# bold = 1
+# dim = 2
+# italic = 3
+# underline = 4
+# blink = 5
+# rapid blink = 6
+# invert = 7
+# hide = 8
+# strike = 9
+
+# 8-bit (256) colors
+# 0-7 = standard
+# 8-15 = bright
+# 16-231 = 216 colors
+# 232-255 = grayscale
+
+# 24-bit colors
+# R,G,B
+# NOT SUPPORTED IN SOME TERMINALS
 
 class Color:
-    """Output Text With Color And Style"""
+    """Output Text With ANSI Color And Style"""
 
-    COLORS = {
-            "black" : 0,
-            "red" : 1,
-            "green": 2,
-            "yellow": 3,
-            "blue" : 4,
-            "magenta" : 5,
-            "cyan": 6,
-            "white": 7,
-            }
-
-    STYLES = {
-            "reset" : 0,
-            "bold" : 1,
-            "dim" : 2,
-            "italic" : 3,
-            "underline" : 4,
-            "blink" : 5,
-            "rapid" : 6, #rapid blink. not usually supported
-            "invert" : 7,
-            "hide" : 8, #hides output
-            "strike" : 9, #not usually supported
-            }
-
-    esc = "\x1b["
-
-    def __init__(self, text=""):
+    def __init__(self, text="", space=" "):
         """Collect Text"""
         self.text = text
+        self.space = space
 
-    def _wrap(self, attr="", num=0, end="m"):
+    def _wrap(self, attr="", num=0, end="m", esc="\x1b["):
         """Wrap with ANSI codes"""
-        self.text = f"{self.esc}{attr}{num}{end}{self.text}{self.esc}0{end}"
+        self.text = f"{esc}{attr}{num}{end}{self.text}{esc}0{end}"
         return self
-
-    # Basic
-    def _color(self, color=""):
-        """Wrap with color"""
-        return self._wrap(3, self.COLORS.get(color, 0))
-
-    def _bright(self, color=""):
-        """Wrap with bright color"""
-        return self._wrap(9, self.COLORS.get(color, 0))
-
-    def _bg(self, color=""):
-        """Wrap with background color"""
-        return self._wrap(4, self.COLORS.get(color, 0))
-
-    def _style(self, style=""):
-        """Wrap with style"""
-        return self._wrap("", self.STYLES.get(style, 0))
-
-    #8-bit (256) colors
-    ###################
-    #0-7 = standard
-    #8-15 = bright
-    #16-231 = 216 colors
-    #232-255 = grayscale
-
-    def extended(self, color=0):
-        """Wrap with extended color (0-255)"""
-        return self._wrap("38;5;", color)
-
-    def on_extended(self, color=0):
-        """Wrap with extended bg color (0-255)"""
-        return self._wrap("48;5;", color)
-
-    # 24-bit colors
-    # R,G,B
-    # NOT SUPPORTED IN SOME TERMINALS
-    def rgb(self, r=0, g=0, b=0):
-        """Wrap with 24-bit color (r,g,b)"""
-        return self._wrap("38;2;", f"{r};{g};{b}")
-
-    def on_rgb(self, r=0, g=0, b=0):
-        """Wrap with 24-bit bg color (r,g,b)"""
-        return self._wrap("48;2;", f"{r};{g};{b}")
-
-    # Shortcut Colors
-    def black(self):
-        """Color Black"""
-        return self._color("black")
-
-    def red(self):
-        """Color Red"""
-        return self._color("red")
-
-    def green(self):
-        """Color Green"""
-        return self._color("green")
-
-    def yellow(self):
-        """Color Yellow"""
-        return self._color("yellow")
-
-    def blue(self):
-        """Color Blue"""
-        return self._color("blue")
-
-    def magenta(self):
-        """Color Magenta"""
-        return self._color("magenta")
-
-    def cyan(self):
-        """Color Cyan"""
-        return self._color("cyan")
-
-    def white(self):
-        """Color White"""
-        return self._color("white")
-
-    # Shortcut Bright Colors
-    def bright_black(self):
-        """Color Bright Black"""
-        return self._bright("black")
-
-    def bright_red(self):
-        """Color Bright Red"""
-        return self._bright("red")
-
-    def bright_green(self):
-        """Color Bright Green"""
-        return self._bright("green")
-
-    def bright_yellow(self):
-        """Color Bright Yellow"""
-        return self._bright("yellow")
-
-    def bright_blue(self):
-        """Color Bright Blue"""
-        return self._bright("blue")
-
-    def bright_magenta(self):
-        """Color Bright Magenta"""
-        return self._bright("magenta")
-
-    def bright_cyan(self):
-        """Color Bright Cyan"""
-        return self._bright("cyan")
-
-    def bright_white(self):
-        """Color Bright White"""
-        return self._bright("white")
-
-    # Shortcut Background
-    def on_black(self):
-        """Background Black"""
-        return self._bg("black")
-
-    def on_red(self):
-        """Background Red"""
-        return self._bg("red")
-
-    def on_green(self):
-        """Background Green"""
-        return self._bg("green")
-
-    def on_yellow(self):
-        """Background Yellow"""
-        return self._bg("yellow")
-
-    def on_blue(self):
-        """Background Blue"""
-        return self._bg("blue")
-
-    def on_magenta(self):
-        """Background Magenta"""
-        return self._bg("magenta")
-
-    def on_cyan(self):
-        """Background Cyan"""
-        return self._bg("cyan")
-
-    def on_white(self):
-        """Background White"""
-        return self._bg("white")
-
-    # Shortcut Styles
-    def reset(self):
-        """Style Reset"""
-        return self._style("reset")
-
-    def bold(self):
-        """Style Bold"""
-        return self._style("bold")
-
-    def dim(self):
-        """Style Dim"""
-        return self._style("dim")
-
-    def blink(self):
-        """Style blink"""
-        return self._style("blink")
-
-    def italic(self):
-        """Style Italic"""
-        return self._style("italic")
-
-    def underline(self):
-        """Style Underline"""
-        return self._style("underline")
-
-    def invert(self):
-        """Style Inverse"""
-        return self._style("invert")
-
-    def hide(self):
-        """Style Hidden"""
-        return self._style("hide")
-
-    def strike(self):
-        """Style Strike"""
-        return self._style("strike")
 
     # Output
     def __add__(self, obj):
         """Concat Text or Color Objs"""
-        self.text = f"{self.text} {obj}"
+        self.text = f"{self.text}{self.space}{obj}"
         return self
 
     def __repr__(self):
         """Return Text"""
         return self.text
+
+    # Colors
+    def black(self):
+        """Color Black"""
+        return self._wrap(3, 0)
+
+    def red(self):
+        """Color Red"""
+        return self._wrap(3, 1)
+
+    def green(self):
+        """Color Green"""
+        return self._wrap(3, 2)
+
+    def yellow(self):
+        """Color Yellow"""
+        return self._wrap(3, 3)
+
+    def blue(self):
+        """Color Blue"""
+        return self._wrap(3, 4)
+
+    def magenta(self):
+        """Color Magenta"""
+        return self._wrap(3, 5)
+
+    def cyan(self):
+        """Color Cyan"""
+        return self._wrap(3, 6)
+
+    def white(self):
+        """Color White"""
+        return self._wrap(3, 7)
+
+    # Bright Colors
+    def bright_black(self):
+        """Color Bright Black"""
+        return self._wrap(9, 0)
+
+    def bright_red(self):
+        """Color Bright Red"""
+        return self._wrap(9, 1)
+
+    def bright_green(self):
+        """Color Bright Green"""
+        return self._wrap(9, 2)
+
+    def bright_yellow(self):
+        """Color Bright Yellow"""
+        return self._wrap(9, 3)
+
+    def bright_blue(self):
+        """Color Bright Blue"""
+        return self._wrap(9, 4)
+
+    def bright_magenta(self):
+        """Color Bright Magenta"""
+        return self._wrap(9, 5)
+
+    def bright_cyan(self):
+        """Color Bright Cyan"""
+        return self._wrap(9, 6)
+
+    def bright_white(self):
+        """Color Bright White"""
+        return self._wrap(9, 7)
+
+    # Background
+    def on_black(self):
+        """Background Black"""
+        return self._wrap(4, 0)
+
+    def on_red(self):
+        """Background Red"""
+        return self._wrap(4, 1)
+
+    def on_green(self):
+        """Background Green"""
+        return self._wrap(4, 2)
+
+    def on_yellow(self):
+        """Background Yellow"""
+        return self._wrap(4, 3)
+
+    def on_blue(self):
+        """Background Blue"""
+        return self._wrap(4, 4)
+
+    def on_magenta(self):
+        """Background Magenta"""
+        return self._wrap(4, 5)
+
+    def on_cyan(self):
+        """Background Cyan"""
+        return self._wrap(4, 6)
+
+    def on_white(self):
+        """Background White"""
+        return self._wrap(4, 7)
+
+    # Styles
+    def reset(self):
+        """Style Reset"""
+        return self._wrap(num=0)
+
+    def bold(self):
+        """Style Bold"""
+        return self._wrap(num=1)
+
+    def dim(self):
+        """Style Dim"""
+        return self._wrap(num=2)
+
+    def italic(self):
+        """Style Italic"""
+        return self._wrap(num=3)
+
+    def underline(self):
+        """Style Underline"""
+        return self._wrap(num=4)
+
+    def blink(self):
+        """Style blink"""
+        return self._wrap(num=5)
+
+    def invert(self):
+        """Style Inverse"""
+        return self._wrap(num=7)
+
+    def hide(self):
+        """Style Hidden"""
+        return self._wrap(num=8)
+
+    def strike(self):
+        """Style Strike"""
+        return self._wrap(num=9)
+
+class Extended(Color):
+    """Extended Color Modes"""
+
+    def color(self, color=0):
+        """Wrap with extended color (0-255)"""
+        return self._wrap("38;5;", color)
+
+    def on_color(self, color=0):
+        """Wrap with extended bg color (0-255)"""
+        return self._wrap("48;5;", color)
+
+class RGB(Color):
+    """RGB Color Modes"""
+
+    def color(self, red=0, green=0, blue=0):
+        """Wrap with 24-bit color (red,green,blue)"""
+        return self._wrap("38;2;", f"{red};{green};{blue}")
+
+    def on_color(self, red=0, green=0, blue=0):
+        """Wrap with 24-bit bg color (red ,green, blue)"""
+        return self._wrap("48;2;", f"{red};{green};{blue}")
 
 if __name__ == "__main__":
     pass
--- tests/test_output.py
+++ tests/test_output.py
@@ -4,12 +4,15 @@
 import unittest
 from colorful import Color
 
+COLORS = ["black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"]
+STYLES = ["reset", "bold", "dim", "italic", "underline", "blink", "invert", "hide", "strike"]
+
 class TestOutputColor(unittest.TestCase):
     """Test Output Base Color"""
 
     title = "Color"
-    check = Color.COLORS
-    method = "_color"
+    check = COLORS
+    method = ""
     method_add = ""
 
     def setUp(self):
@@ -31,85 +34,29 @@ class TestOutputColor(unittest.TestCase):
                 else:
                     print(f"X.) *Skipping* ({self.method_add}{itm} method not found)")
 
-
-class TestOutputColorWrap(TestOutputColor):
-    """Test Output Base Color Wrapper"""
-
-    title = "Color Wrap"
-    check = Color.COLORS
-    method = ""
-    method_add = ""
-
-
 class TestOutputStyle(TestOutputColor):
     """Test Output Style"""
 
     title = "Style"
-    check = Color.STYLES
-    method = "_style"
-    method_add = ""
-
-
-class TestOutputStyleWrapper(TestOutputColor):
-    """Test Output Style Wrapper"""
-
-    title = "Style Wrapper"
-    check = Color.STYLES
+    check = STYLES
     method = ""
     method_add = ""
-
 
 class TestOutputBG(TestOutputColor):
     """Test Output BG"""
 
     title = "Backgrounds"
-    check = Color.COLORS
-    method = "_bg"
-    method_add = ""
-
-
-class TestOutputBGWrapper(TestOutputColor):
-    """Test Output BG Wrapper"""
-
-    title = "BG Wrapper"
-    check = Color.COLORS
+    check = COLORS
     method = ""
     method_add = "on_"
-
 
 class TestOutputBright(TestOutputColor):
     """Test Output Bright"""
 
     title = "Bright"
-    check = Color.COLORS
-    method = "_bright"
-    method_add = ""
-
-class TestOutputBrightWrapper(TestOutputColor):
-    """Test Output Bright Wrapper"""
-
-    title = "Bright Wrapper"
-    check = Color.COLORS
+    check = COLORS
     method = ""
     method_add = "bright_"
-
-class TestOutputAdd(TestOutputColor):
-    """Test Output Class Adding"""
-
-    title = "Class Adding"
-    check = ()
-    method = ""
-    method_add = ""
-
-    def test_loop(self):
-        """Loop through colors and print"""
-        for index, color in enumerate(Color.COLORS):
-            obj1 = Color(f"{index+1}.)").bold()._bg(color)
-            obj2 = Color(f"{color}")._color(color)
-            num = 12345
-            string = "String"
-            print(obj1+obj2+num+string)
-
 
 if __name__ == "__main__":
     unittest.main()
